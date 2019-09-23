29e386f72c44835b15b630d3881f892943cfe49b
AWolf81 <AWolf2904@gmail.com>
2019-09-23 21:39:08 +0200

    Add AddThis Theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,20 @@
+const assert = require('assert')
+require('dotenv').config()
+
+assert(process.env.ADDTHIS_PUBLIC_ID, "Please add ADDTHIS_PUBLIC_ID to .env so AddThis Share will work");
+
 module.exports = {
   plugins: [
     {
       resolve: `gatsby-theme-blog`,
       options: {},
     },
+    { 
+      resolve: `@awolf81/gatsby-theme-addthis`, 
+      options: {
+        publicId: process.env.ADDTHIS_PUBLIC_ID
+      } 
+    }
   ],
   // Customize your site metadata:
   siteMetadata: {
